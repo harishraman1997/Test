cfb0e1996494c701f662243567dba6ad50fa6120
harishraman1997 <harishootty@gmail.com>
2023-04-29 20:13:41 +0530

    second

--- src/com/test/SampleTest.java
+++ src/com/test/SampleTest.java
@@ -3,6 +3,7 @@ package com.test;
 public class SampleTest {
 	public static void main(String[] args) {
 		int a=10;
+		int b =20;
 		System.out.println("gggg");
 		System.out.println("esdg");
 		System.out.println("ojjeiuhurh");
