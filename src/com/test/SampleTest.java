fcf1002be4ebc16e69ddeeb47993cfe9eccb3cc4
harishraman1997 <harishootty@gmail.com>
2023-04-29 20:55:49 +0530

    pull

--- src/com/test/SampleTest.java
+++ src/com/test/SampleTest.java
@@ -2,9 +2,6 @@ package com.test;
 
 public class SampleTest {
 	public static void main(String[] args) {
-		System.out.println("gggg");
-		System.out.println("esdg");
-		System.out.println("ojjeiuhurh");
-	}
+		
 
-}
+}}
