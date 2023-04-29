e45f287febe1198b6def80ae485e487f98070cde
harishraman1997 <131872211+harishraman1997@users.noreply.github.com>
2023-04-29 20:05:53 +0530

    Update SampleTest.java

--- src/com/test/SampleTest.java
+++ src/com/test/SampleTest.java
@@ -2,6 +2,7 @@ package com.test;
 
 public class SampleTest {
 	public static void main(String[] args) {
+		int a=10;
 		System.out.println("gggg");
 		System.out.println("esdg");
 		System.out.println("ojjeiuhurh");
