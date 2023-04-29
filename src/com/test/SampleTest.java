019589f7d864c2176b036e489436d7cc26fc4b36
harishraman1997 <harishootty@gmail.com>
2023-04-29 18:54:16 +0530

    sample work push

--- src/com/test/SampleTest.java
+++ src/com/test/SampleTest.java
@@ -3,7 +3,8 @@ package com.test;
 public class SampleTest {
 	public static void main(String[] args) {
 		System.out.println("gggg");
-		System.out.println("hvhjvjh");
+		System.out.println("esdg");
+		System.out.println("ojjeiuhurh");
 	}
 
 }
